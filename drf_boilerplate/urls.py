db61b791f2091e440787e2f9e0829978fde5aa98
Brandon Brown <brandon.brown@ymail.com>
2015-05-26 11:14:52 -0700

    implements a single route with basic template

--- drf_boilerplate/urls.py
+++ drf_boilerplate/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from jsframework import urls as jsf_urls
 
 urlpatterns = [
+    url(r'^', include(jsf_urls)),
     url(r'^admin/', include(admin.site.urls)),
 ]
