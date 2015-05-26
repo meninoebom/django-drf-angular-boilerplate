65d32e6ee37cb1f25e4d26c80d75100c47da4190
Brandon Brown <brandon.brown@ymail.com>
2015-05-26 14:38:29 -0700

    adds angular, angular-ui-router, and a single ui-view rendered within a single django template

---
+++ README.md
@@ -0,0 +1,4 @@
+Getting Started
+
+  Change the name of  the app 'jsframework' to an appropriate name for your new Django project.
+  Change the path to files in 'jsframework' in the url paths of <i>templates/index.html</i> and in <i>.bowerrc</i>
--- jsframework/static/jsframework/js/index.js
+++ jsframework/static/jsframework/js/index.js
@@ -1,1 +1,12 @@
-console.log('foo bar oipadvbiopads');
+var App = angular.module('App', ['ui.router']);
+
+
+App.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/");
+
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "/static/jsframework/js/views/home.html"
+    });
+});
